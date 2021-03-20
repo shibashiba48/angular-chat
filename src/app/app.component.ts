1039b392951bf0a07f5f89fafe267033426cc14a
shibashiba48 <t1257476@gmail.com>
2021-03-21 06:34:36 +0900

    タイムライン表示

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+import { Comment } from './class/comment';
+
+const COMMENTS: Comment[] = [
+  { name: '山田太郎', message: 'おつです！'},
+  { name: '山田太郎', message: 'お腹すきましたね！'},
+  { name: '木場嵐土', message: 'おつです！'},
+];
+
 @Component({
   selector: 'ac-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-chat';
+  comments = COMMENTS;
 }
