1f3ae51f954632b669c9e89328ca504272109cb1
shibashiba48 <t1257476@gmail.com>
2021-03-28 11:34:32 +0900

    チャットコンポーネントへ分割

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,52 +1,11 @@
 import { Component } from '@angular/core';
-import { AngularFireDatabase, AngularFireList, SnapshotAction } from '@angular/fire/database';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-
-import { Comment } from './class/comment';
-import { User } from './class/user';
-
-const CURRENT_USER: User = new User(1, '木場嵐土');
-const ANOTHER_USER: User = new User(2, 'パワプロ');
 
 @Component({
   selector: 'ac-root',
-  templateUrl: './app.component.html',
+  template: `
+    <ac-chat></ac-chat>
+    `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  comments$: Observable<Comment[]>;
-  commentsRef: AngularFireList<Comment>;
-  currentUser = CURRENT_USER;
-  comment = '';
-
-  constructor(private db: AngularFireDatabase) {
-    this.commentsRef = db.list('/comments');
-    this.comments$ = this.commentsRef.snapshotChanges()
-      .pipe(
-        map((snapshots: SnapshotAction<Comment>[]) => {
-          return snapshots.map(snapshot => {
-            const value = snapshot.payload.val();
-            return new Comment({key: snapshot.payload.key, ...value});
-          });
-        })
-      )
-  }
-
-  addComment(comment: string): void {
-    if (comment) {
-      this.commentsRef.push(new Comment({ user: this.currentUser, message: comment }));
-      this.comment = '';
-    }
-  }
-
-  updateComment(comment: Comment): void {
-    const { key, message } = comment;
-
-    this.commentsRef.update(key, { message });
-  }
-
-  deleteComment(comment: Comment): void {
-    this.commentsRef.remove(comment.key);
-  }
 }
